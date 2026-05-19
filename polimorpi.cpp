30b5e109a5f2c2c5351fad1947237ddc366fe905
variannn340 <varianvarian0301@gmail.com>
2026-05-19 10:52:50 +0700

    joko untuk class seseorang

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -10,3 +10,12 @@ public:
         cout << "Pesan dari seseorang" << endl;
     }
 };
+
+class joko : public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari joko" << endl;
+    }
+};
