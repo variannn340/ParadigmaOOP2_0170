17e56c22d026ad25ac46752d6e1784f74e848109
variannn340 <varianvarian0301@gmail.com>
2026-05-19 10:42:52 +0700

    Menambah nilai pada saklarNo

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class remoteLampu
+{
+private:
+    string saklarNo[10];
+
+public:
+    void setSaklarNo(int i, string value)
+    {
+        saklarNo[i] = value;
+    }
+    string getSatklarNo(int i)
+    {
+        return saklarNo[i];
+    }
+};
