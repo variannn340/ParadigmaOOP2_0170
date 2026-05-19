c41a95e5c03ad7de7c3d8ea5787e0e2ccae3f873
variannn340 <varianvarian0301@gmail.com>
2026-05-19 10:53:53 +0700

    lia untuk class seseorang

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -19,3 +19,12 @@ public:
         cout << "Pesan dari joko" << endl;
     }
 };
+
+class lia : public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari lia" << endl;
+    }
+};
