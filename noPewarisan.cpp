f87ad5930bff262746a777a3eddc3ed9740031ab
variannn340 <varianvarian0301@gmail.com>
2026-05-19 11:14:54 +0700

    deprivedClass untuk baseClass1

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -13,3 +13,9 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
+class deprivedClass : public baseClass1 {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari deprived class";
+    }
+};
